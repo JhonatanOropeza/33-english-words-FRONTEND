be0e16284dba279f863f00abae0694e1a15447ac
Jhon <jhon_oropeza810otmail.com>
2021-03-03 14:01:04 -0600

    Prueba video sin transparencia 4 (sin video-reat)

--- src/components/2_Access/PrincipalNoLogin.js
+++ src/components/2_Access/PrincipalNoLogin.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Player, ControlBar } from 'video-react';
-
+//import { Player, ControlBar } from 'video-react';
+import video from '../../Videos/videoLog.mp4';
 //import First from './first';
 //import Login from './login';
 //import Logup from './logup';
@@ -22,10 +22,13 @@ export default class PrincipalNoLogin extends Component {
                 <div className="ToAccess-Transparency-2"></div>
                 */}
                 <div className="ToAccess-Video-3">
+                    <video src={video} type="video/mp4" autoPlay muted/>
+                    {/** 
                     <Player loop autoPlay muted>
                         <ControlBar disableDefaultControls disableCompletely />
-                        <source src="/Videos/video1.mp4" type="video/mp4" muted="true"/>
+                        <source src="/Videos/video1.mp4" type="video/mp4"/>
                     </Player>
+                    */}
                 </div>
             </div>
         )
